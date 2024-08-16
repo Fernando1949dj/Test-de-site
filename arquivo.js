1b70d7cf462de7ea32c0f252db29417aa36093a2
Fernando1949dj <155067398+Fernando1949dj@users.noreply.github.com>
2024-08-16 14:48:41 -0300

    Create arquivo.js

---
+++ arquivo.js
@@ -0,0 +1,9 @@
+function getLista() {
+  return [
+    { texto: "Olá, como você está?", lado: "esquerda" },
+    { texto: "Estou bem, obrigado!", lado: "direita" },
+    { texto: "Que bom!", lado: "esquerda" },
+    { texto: "Sim, estou feliz!", lado: "direita" },
+    { texto: "Isso é ótimo!", lado: "esquerda" },
+  ];
+}
